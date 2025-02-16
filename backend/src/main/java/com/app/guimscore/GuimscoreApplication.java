7d172eee0920941029ff012b62a24802ff8193ed
Faguim02 <fagnermuniz12@gmail.com>
2025-02-15 22:42:05 -0300

    ✨ feat: adicionando as ferramentas

---
+++ backend/src/main/java/com/app/guimscore/GuimscoreApplication.java
@@ -0,0 +1,13 @@
+package com.app.guimscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuimscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuimscoreApplication.class, args);
+	}
+
+}
