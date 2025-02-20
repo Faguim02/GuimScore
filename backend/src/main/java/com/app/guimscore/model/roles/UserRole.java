de84ba401eb729f5a4c10a86090315335a2d752c
Faguim02 <fagnermuniz12@gmail.com>
2025-02-20 10:59:13 -0300

    ✨ feat: adicionando os tipos de usuario

--- backend/src/main/java/com/app/guimscore/model/roles/UserRole.java
+++ backend/src/main/java/com/app/guimscore/model/roles/UserRole.java
@@ -1,4 +1,15 @@
 package com.app.guimscore.model.roles;
 
 public enum UserRole {
+    ADMIN("admin"),
+    PLAYER("player");
+
+    private String userRole;
+    UserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
 }
