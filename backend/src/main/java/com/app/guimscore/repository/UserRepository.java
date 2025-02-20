85a6440fc9500c6b0e7b5d2b96295be51ffb333d
Faguim02 <fagnermuniz12@gmail.com>
2025-02-20 10:58:02 -0300

    ✨ feat: adicionando os metodos 'existsByName()' e 'findByName()'

--- backend/src/main/java/com/app/guimscore/repository/UserRepository.java
+++ backend/src/main/java/com/app/guimscore/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.app.guimscore.repository;
 
 import com.app.guimscore.model.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends MongoRepository<UserModel, UUID> {
+    boolean existsByName(String name);
+    UserDetails findByName(String name);
 }
