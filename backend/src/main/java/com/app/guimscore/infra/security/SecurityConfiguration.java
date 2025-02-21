e131d807a4f1ac8b7d187f674bc1b2cb05d3a6c6
Faguim02 <fagnermuniz12@gmail.com>
2025-02-20 22:38:37 -0300

    🔧 chore: configurando midleware que verifica o token

--- backend/src/main/java/com/app/guimscore/infra/security/SecurityConfiguration.java
+++ backend/src/main/java/com/app/guimscore/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.app.guimscore.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,6 +29,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorization -> {
                     authorization.requestMatchers("auth/**").permitAll();
                 })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
