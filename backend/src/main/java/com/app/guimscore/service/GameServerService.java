28ac0c4354e219abb85b509655f0eac04b9f4c5d
Faguim02 <fagnermuniz12@gmail.com>
2025-04-15 09:39:12 -0300

    ✨ feat: arquivo GameServerServce criado

---
+++ backend/src/main/java/com/app/guimscore/service/GameServerService.java
@@ -0,0 +1,10 @@
+package com.app.guimscore.service;
+
+import com.app.guimscore.repository.GameServerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GameServerService {
+    @Autowired
+    private GameServerRepository gameServerRepository;
+
+}
