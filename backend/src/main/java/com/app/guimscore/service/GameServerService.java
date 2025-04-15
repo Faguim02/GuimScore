21d543d397ac9d115d085adee5725f043f32e7ad
Faguim02 <fagnermuniz12@gmail.com>
2025-04-15 09:50:30 -0300

    ✨ feat: foi criado a função 'createGameServer()' que cria um novo gameServer

--- backend/src/main/java/com/app/guimscore/service/GameServerService.java
+++ backend/src/main/java/com/app/guimscore/service/GameServerService.java
@@ -1,10 +1,38 @@
 package com.app.guimscore.service;
 
+import com.app.guimscore.dto.GameServerDto;
+import com.app.guimscore.model.GameServerModel;
+import com.app.guimscore.model.UserModel;
+import com.app.guimscore.model.exceptions.NotFoundException;
 import com.app.guimscore.repository.GameServerRepository;
+import com.app.guimscore.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class GameServerService {
+
     @Autowired
     private GameServerRepository gameServerRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createGameServer(GameServerDto gameServerDto, UUID userId) {
+
+        Optional<UserModel> userModel = this.userRepository.findById(userId);
+
+        if (userModel.isEmpty()) {
+            throw new NotFoundException("Usuario inexistente");
+        }
+
+        GameServerModel gameServerModel = new GameServerModel();
+
+        BeanUtils.copyProperties(gameServerDto, gameServerModel);
+
+        gameServerModel.setUser(userModel.get());
+
+    }
 
 }
