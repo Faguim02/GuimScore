ba8580d24c0625e6b340dd5e8ca7fb8827292e1d
Faguim02 <fagnermuniz12@gmail.com>
2025-05-12 12:54:32 -0300

    ✨ feat: foi criado uma função de interface que busca todos os Datas pelo gameserver

--- backend/src/main/java/com/app/guimscore/repository/DataRepository.java
+++ backend/src/main/java/com/app/guimscore/repository/DataRepository.java
@@ -1,9 +1,15 @@
 package com.app.guimscore.repository;
 
 import com.app.guimscore.model.DataModel;
+import com.app.guimscore.model.GameServerModel;
+import com.app.guimscore.model.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DataRepository extends MongoRepository<DataModel, UUID> {
+
+    List<DataModel> findByGameServerModel(GameServerModel gameServerModel);
+
 }
