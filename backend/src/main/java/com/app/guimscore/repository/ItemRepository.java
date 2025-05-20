0ba0a5980f789eaaec441f63581254f274bf1961
Faguim02 <fagnermuniz12@gmail.com>
2025-05-20 14:40:44 -0300

    ✨ feat: foi criado uma função que busca os items pela lista

--- backend/src/main/java/com/app/guimscore/repository/ItemRepository.java
+++ backend/src/main/java/com/app/guimscore/repository/ItemRepository.java
@@ -1,9 +1,12 @@
 package com.app.guimscore.repository;
 
 import com.app.guimscore.model.ItemModel;
+import com.app.guimscore.model.ItemsModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ItemRepository extends MongoRepository<ItemModel, UUID> {
+    List<ItemModel> findByListItem(ItemsModel listItem);
 }
