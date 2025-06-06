832dea92eee1058b487950d45ec993e49a146434
Faguim02 <fagnermuniz12@gmail.com>
2025-06-06 13:26:18 -0300

    ✨ feat: foi criado uma rota get para retornar todos os gameServers

--- backend/src/main/java/com/app/guimscore/view/controller/GameServerController.java
+++ backend/src/main/java/com/app/guimscore/view/controller/GameServerController.java
@@ -4,16 +4,16 @@ import com.app.guimscore.dto.GameServerDto;
 import com.app.guimscore.infra.security.JwtService;
 import com.app.guimscore.service.GameServerService;
 import com.app.guimscore.view.model.GameServerReqDto;
+import com.app.guimscore.view.model.GameServerResDto;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -34,7 +34,22 @@ public class GameServerController {
 
         this.gameServerService.createGameServer(gameServerDto, userId);
 
-        return ResponseEntity.ok("GameServer criado com sucesso");
+        return ResponseEntity.status(HttpStatus.CREATED).body("GameServer criado com sucesso");
+
+    }
+
+    @GetMapping()
+    ResponseEntity<List<GameServerResDto>> findAllGameServer(Authentication authentication) {
+
+        UUID userId = this.jwtService.getUserIdByToken(authentication);
+
+        List<GameServerDto> gameServerDtoList = this.gameServerService.findAllGameServers(userId);
+
+        List<GameServerResDto> gameServerResDtoList = gameServerDtoList.stream()
+                .map(game -> new GameServerResDto(game.getUuid(), game.getNameServer(), game.getDescription(), game.getDateAt()))
+                .toList();
+
+        return ResponseEntity.ok(gameServerResDtoList);
 
     }
 
