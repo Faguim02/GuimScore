71dcb093ddcefd87966045e7ea485762a336152c
Faguim02 <fagnermuniz12@gmail.com>
2025-08-03 18:12:07 -0300

    fix(backend): adjust spring security for cors preflight

--- backend/src/main/java/com/app/guimscore/infra/security/SecurityConfiguration.java
+++ backend/src/main/java/com/app/guimscore/infra/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.app.guimscore.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,10 +26,13 @@ public class SecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> new org.springframework.web.cors.CorsConfiguration().applyPermitDefaultValues())) // Garante que a config CORS seja usada
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorization -> {
-                    authorization.requestMatchers("api/auth/**").permitAll();
-                    authorization.anyRequest().hasRole("ADMIN");
+                    
+			authorization.requestMatchers("api/auth/**").permitAll();
+            authorization.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+			authorization.anyRequest().hasRole("ADMIN");
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
