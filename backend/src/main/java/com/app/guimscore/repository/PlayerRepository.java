995b2603114b2aaf88456a806d37d23c5e0b20cc
Faguim02 <fagnermuniz12@gmail.com>
2025-09-23 13:40:06 -0300

    feat: adicionando metodos para buscar player pelo nome e lista de players pelo id do game server

--- backend/src/main/java/com/app/guimscore/repository/PlayerRepository.java
+++ backend/src/main/java/com/app/guimscore/repository/PlayerRepository.java
@@ -4,9 +4,12 @@ import com.app.guimscore.model.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PlayerRepository extends MongoRepository<Player, UUID> {
-    Player findByName(String name);
+    Optional<Player> findByName(String name);
+    List<Player> findByGameServerId(UUID gameServerId);
 }
