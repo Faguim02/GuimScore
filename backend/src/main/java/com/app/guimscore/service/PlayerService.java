f1a28c2d81131a9b01e2b916d10e3059da08a6f9
Faguim02 <fagnermuniz12@gmail.com>
2025-09-23 13:42:20 -0300

    ♻️ refactor: alterações

--- backend/src/main/java/com/app/guimscore/service/PlayerService.java
+++ backend/src/main/java/com/app/guimscore/service/PlayerService.java
@@ -1,14 +1,20 @@
 package com.app.guimscore.service;
 
 import com.app.guimscore.dto.PlayerDto;
+import com.app.guimscore.model.DataModel;
+import com.app.guimscore.model.GameServerModel;
 import com.app.guimscore.model.Player;
+import com.app.guimscore.model.exceptions.ForbiddenException;
 import com.app.guimscore.model.exceptions.NotFoundException;
+import com.app.guimscore.repository.DataRepository;
+import com.app.guimscore.repository.GameServerRepository;
 import com.app.guimscore.repository.PlayerRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,11 +25,17 @@ public class PlayerService {
     @Autowired
     private PlayerRepository playerRepository;
 
-    public PlayerDto signUp(PlayerDto player, UUID gameServerId) {
+    @Autowired
+    private DataRepository dataRepository;
+
+    @Autowired
+    private GameServerRepository gameServerRepository;
+
+    public PlayerDto signUp(PlayerDto player) {
 
         try {
 
-            if (player.getName() == null || player.getPassword() == null) {
+            if (player.getName() == null || player.getPassword() == null || player.getGameServerId() == null) {
                 throw new NotFoundException("Nome ou senha nulos");
             }
 
@@ -31,14 +43,18 @@ public class PlayerService {
                 throw new NotFoundException("Nome ou senha vazios");
             }
 
-            player.setGameServerId(gameServerId);
+            //player.setGameServerId(player.getGameServerId());
 
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             String hashedPassword = passwordEncoder.encode(player.getPassword());
             player.setPassword(hashedPassword);
 
-            Player playerModel = new Player();
+            //
+            GameServerModel gameServerModel = gameServerRepository.findById(player.getGameServerId()).orElseThrow(() -> new NotFoundException("Servidor de jogo não encontrado"));
+            List<DataModel> dataModelList = dataRepository.findByGameServerModel(gameServerModel);
 
+            Player playerModel = new Player();
+            playerModel.setData(dataModelList);
             BeanUtils.copyProperties(player, playerModel);
 
             playerRepository.save(playerModel);
@@ -54,18 +70,18 @@ public class PlayerService {
 
     }
 
-    public PlayerDto findByName(PlayerDto player) {
+    public PlayerDto signIn(PlayerDto player) {
 
         try {
 
-            Optional<Player> playerModel = Optional.ofNullable(playerRepository.findByName(player.getName()));
+            Optional<Player> playerModel = playerRepository.findByName(player.getName());
             if (playerModel.isEmpty()) {
                 throw new NotFoundException("Jogador não encontrado");
             }
 
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             if (!passwordEncoder.matches(player.getPassword(), playerModel.get().getPassword())) {
-                throw new NotFoundException("Senha incorreta");
+                throw new ForbiddenException("Senha incorreta");
             }
 
             PlayerDto playerDto = new PlayerDto();
@@ -74,6 +90,10 @@ public class PlayerService {
 
             return playerDto;
 
+        } catch (NotFoundException notFoundException) {
+            throw new NotFoundException(notFoundException.getMessage());
+        } catch (ForbiddenException forbiddenException) {
+            throw new ForbiddenException(forbiddenException.getMessage());
         } catch (Exception e) {
             throw new RuntimeException("Erro ao buscar jogador");
         }
