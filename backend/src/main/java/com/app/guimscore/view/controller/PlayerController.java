8d156e91adad900c19ddc93acf93a21a884662dc
Faguim02 <fagnermuniz12@gmail.com>
2025-09-30 13:31:16 -0300

    feat: adicionando rota de increment e decrement

--- backend/src/main/java/com/app/guimscore/view/controller/PlayerController.java
+++ backend/src/main/java/com/app/guimscore/view/controller/PlayerController.java
@@ -3,11 +3,14 @@ package com.app.guimscore.view.controller;
 import com.app.guimscore.dto.PlayerDto;
 import com.app.guimscore.model.DataModel;
 import com.app.guimscore.model.ItemsModel;
+import com.app.guimscore.service.DataService;
 import com.app.guimscore.service.PlayerService;
 import com.app.guimscore.view.model.PlayerDetailsRes;
 import com.app.guimscore.view.model.PlayerReqDto;
 import com.app.guimscore.view.model.PlayerResDto;
+import com.app.guimscore.view.model.ValueToDataReqDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +25,11 @@ public class PlayerController {
 
     @Autowired
     private PlayerService playerService;
+    @Autowired
+    private DataService dataService;
 
     @PostMapping("/signUp")
-    public void signUp(@RequestBody PlayerReqDto playerReqDto) {
+    public ResponseEntity<Void> signUp(@RequestBody PlayerReqDto playerReqDto) {
         // Implementation for player sign-up
 
         PlayerDto playerDto = new PlayerDto(playerReqDto.name(), playerReqDto.password(), playerReqDto.gameServerId());
@@ -41,6 +46,8 @@ public class PlayerController {
         }
 
          this.playerService.signUp(playerDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/login")
@@ -60,6 +67,22 @@ public class PlayerController {
         PlayerDetailsRes playerDetailsRes = new PlayerDetailsRes(playerDto.getId(), playerDto.getName(), playerDto.getDateOfBirth(), playerDto.getGameServerId(), playerDto.getItems(), playerDto.getData());
         //PlayerDetailsRes playerDetailsRes = new PlayerDetailsRes(UUID.randomUUID(), "playerDto.getName()", new Date(), UUID.randomUUID(), List.of(new ItemsModel()), List.of(new DataModel()));
         return ResponseEntity.ok(playerDetailsRes);
+    }
+
+    @PutMapping("/addValue")
+    ResponseEntity<String> incrementData(@RequestBody ValueToDataReqDto valueToData) {
+
+        this.dataService.addValueToData(valueToData.value(), valueToData.dataId(), valueToData.gameId(), valueToData.playerId());
+
+        return ResponseEntity.ok("Um dado foi incrementado");
+
+    }
+
+    @PutMapping("/subtractValue")
+    ResponseEntity<String> decrementData(@RequestBody ValueToDataReqDto valueToData) {
+        this.dataService.subtractValueToData(valueToData.value(), valueToData.dataId(), valueToData.gameId(), valueToData.playerId());
+
+        return ResponseEntity.ok("Um dado foi decrementado");
     }
 
 }
