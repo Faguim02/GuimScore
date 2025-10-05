f1d72c3608045b8d2da1eaabfd5459e6322dd5c6
Faguim02 <fagnermuniz12@gmail.com>
2025-10-04 22:05:30 -0300

    feat: criando um novo model para as apiskeys

---
+++ backend/src/main/java/com/app/guimscore/model/ApiKey.java
@@ -0,0 +1,51 @@
+package com.app.guimscore.model;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class ApiKey {
+    private UUID id;
+    private String key;
+    private String name;
+    private Date createdData;
+
+    public ApiKey(String name, String key) {
+        this.id = UUID.randomUUID();
+        this.key = key;
+        this.name = name;
+        this.createdData = new Date();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedData() {
+        return createdData;
+    }
+    public void setCreatedData(Date createdData) {
+        this.createdData = createdData;
+    }
+}
