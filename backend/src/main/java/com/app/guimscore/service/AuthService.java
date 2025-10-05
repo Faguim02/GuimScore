2c56156999a90dc1c9af0aaa7b02ed5c9c5c16fb
Faguim02 <fagnermuniz12@gmail.com>
2025-10-04 22:07:02 -0300

    feat: adicionando funções de gerar e verificar keys

--- backend/src/main/java/com/app/guimscore/service/AuthService.java
+++ backend/src/main/java/com/app/guimscore/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.app.guimscore.service;
 
+import com.app.guimscore.model.ApiKey;
+import com.app.guimscore.model.exceptions.NotFoundException;
 import com.app.guimscore.model.exceptions.UnprocessableEntityException;
 import com.app.guimscore.view.model.SignInResDto;
 import com.app.guimscore.view.model.SignUpResDto;
@@ -14,10 +16,13 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -68,4 +73,49 @@ public class AuthService {
         }
     }
 
+    public String createApiKey(UUID userId, String nameApiKey) {
+        UserModel userModel = this.userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Usuario inexistente"));
+
+        if (nameApiKey == null || nameApiKey.isBlank()) {
+            throw new NotFoundException("Nome da Api key nulo ou vazio");
+        }
+
+        String[] keys = this.generateKeyEncoded(nameApiKey);
+
+        ApiKey apiKey = new ApiKey(nameApiKey, keys[1]);
+
+        userModel.addApiKey(apiKey);
+
+        System.out.println("aa");
+        this.userRepository.save(userModel);
+
+        return keys[0];
+    }
+
+    public boolean validateApiKey(String apiKey, String userName) {
+        if (apiKey == null || apiKey.isBlank()) {
+            throw new NotFoundException("Api key nula ou vazia");
+        }
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        UserModel userModel = (UserModel) this.userRepository.findByName(userName);
+
+        for (ApiKey key : userModel.getApiKeys()) {
+            if (passwordEncoder.matches(apiKey, key.getKey())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String[] generateKeyEncoded(String name) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawKey = UUID.randomUUID().toString() + name + new Date().getTime();
+
+        String keyEncoded = passwordEncoder.encode(rawKey);
+
+        return new String[]{rawKey, keyEncoded};
+    }
 }
